c40fbd2566a385b92825744a51c1317d44c9b2cb
嗚呼起飛 <zxr52090114@gmail.com>
2025-06-02 18:45:34 +0800

    Initial Commit

---
+++ IOS_final_game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS_final_game
+//
+//  Created by 11144137 on 2025/6/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS_final_game/IOS_final_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_final_gameApp.swift
+//  IOS_final_game
+//
+//  Created by 11144137 on 2025/6/2.
+//
+
+import SwiftUI
+
+@main
+struct IOS_final_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
